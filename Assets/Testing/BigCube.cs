b83cecdc07e9b28da963d1a7bc1adfa339359de9
Matthew Morris <MRRMAT011@myuct.ac.za>
2020-03-17 14:33:07 +0200

    Testing for child movement propogation working

---
+++ Assets/Testing/BigCube.cs
@@ -0,0 +1,21 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigCube : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        if (Input.GetKeyDown("space")) transform.Translate(0, 0, 1);
+    }
+}
---
+++ Assets/Testing/SmallCube.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer()) return;
+
+        if (Input.GetKeyDown("a")) transform.Translate(1, 0, 0);
+        if (Input.GetKeyDown("d")) transform.Translate(-1, 0, 0);
+    }
+
+    bool isLocalPlayer()
+    {
+        return gameObject.transform.parent.gameObject.GetComponent<NetworkIdentity>().isLocalPlayer;
+    }
+}
