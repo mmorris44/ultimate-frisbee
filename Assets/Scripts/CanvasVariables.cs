f02dbb3f8c0d68d13ed20cd5fdea497a9d06c29a
Matthew Morris <MRRMAT011@myuct.ac.za>
2020-04-29 21:30:03 +0200

    Stat setting

--- Assets/Scripts/CanvasVariables.cs
+++ Assets/Scripts/CanvasVariables.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Mirror;
+using UnityEngine.UI;
 
 public class CanvasVariables : MonoBehaviour
 {
+    public int maxPoints = 5;
+    public bool validPlayerSetup = true;
+
     public TMP_Dropdown teamDropdown;
     public TMP_InputField nameField;
     public NetworkManager networkManager;
 
+    public TMP_Dropdown agilityDropdown;
+    public TMP_Dropdown speedDropdown;
+    public TMP_Dropdown enduranceDropdown;
+    public TMP_Dropdown handlingDropdown;
+    public Text totalText;
+
     public GameObject ingameDisplay;
     public GameObject pregameOptions;
+
+    void Start()
+    {
+        OnStatDropdownChange();
+    }
 
     void Update()
     {
@@ -26,5 +41,40 @@ public class CanvasVariables : MonoBehaviour
             pregameOptions.SetActive(false);
             ingameDisplay.SetActive(true);
         }
+    }
+
+    // Update validity and text
+    public void OnStatDropdownChange()
+    {
+        // Set text
+        int totalPointsUsed = TotalPointsUsed();
+        totalText.text = TotalText(totalPointsUsed);
+
+        // Set color and validity
+        if (totalPointsUsed > maxPoints)
+        {
+            validPlayerSetup = false;
+            totalText.color = Color.red;
+        }
+        else if (totalPointsUsed == maxPoints)
+        {
+            validPlayerSetup = true;
+            totalText.color = Color.green;
+        }
+        else if(totalPointsUsed < maxPoints)
+        {
+            validPlayerSetup = true;
+            totalText.color = Color.black;
+        }
+    }
+
+    private string TotalText(int pointsUsed)
+    {
+        return "= " + pointsUsed + "/" + maxPoints;
+    }
+
+    private int TotalPointsUsed()
+    {
+        return 8 - agilityDropdown.value - speedDropdown.value - enduranceDropdown.value - handlingDropdown.value;
     }
 }
---
+++ Assets/Scripts/Player/PlayerPresets.cs
@@ -0,0 +1,64 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPresets : NetworkBehaviour
+{
+    // Preset values
+
+    // Agility
+    public float[] layoutSpeed = { 13f, 10f, 7f }; // Layout speed
+    public float[] layoutRecovery = { 1.5f, 2f, 2.5f }; // Layout recovery time
+    public float[] turnSpeed = { 100f, 80f, 60f }; // Rotation speed
+
+    // Speed
+    public float[] topSpeed = { 3.5f, 3f, 2.5f }; // Top speed
+    public float[] shuffleSpeed = { 2.5f, 2f, 1.5f }; // Movement on spot
+
+    // Handling
+    public float[] pivotHorizontalReach = { 1.2f, 0.9f, 0.4f }; // Max 1.2, min 0.4
+    public float[] pivotVerticalReach = { 0.5f, 0.3f, 0.1f }; // Normal height = 1.5, min height = 1, max height 2. So max vertical reach is 0.5
+    public float[] reach = { 7f, 5f, 4f }; // Reach when catching
+
+    // Endurance
+    public float[] maxEnergy = { 500f, 400f, 300f }; // Max energy for sprinting and laying out
+    public float[] energyRecovery = { 0.7f, 0.5f, 0.4f }; // Energy recovered per frame
+
+    public PlayerController playerController;
+
+    private CanvasVariables canvasVariables;
+
+    void Start()
+    {
+        if (!isLocalPlayer) return;
+        canvasVariables = GameObject.Find("/GameControl").GetComponent<CanvasVariables>();
+
+        // Set to default if not valid
+        if (!canvasVariables.validPlayerSetup)
+        {
+            canvasVariables.agilityDropdown.value = 1;
+            canvasVariables.speedDropdown.value = 1;
+            canvasVariables.handlingDropdown.value = 1;
+            canvasVariables.enduranceDropdown.value = 1;
+        }
+
+        // Agility
+        playerController.layoutSpeed = layoutSpeed[canvasVariables.agilityDropdown.value];
+        playerController.layoutRecovery = layoutRecovery[canvasVariables.agilityDropdown.value];
+        playerController.turnSpeed = turnSpeed[canvasVariables.agilityDropdown.value];
+
+        // Speed
+        playerController.topSpeed = topSpeed[canvasVariables.speedDropdown.value];
+        playerController.shuffleSpeed = shuffleSpeed[canvasVariables.speedDropdown.value];
+
+        // Handling
+        playerController.pivotHorizontalReach = pivotHorizontalReach[canvasVariables.handlingDropdown.value];
+        playerController.pivotVerticalReach = pivotVerticalReach[canvasVariables.handlingDropdown.value];
+        playerController.reach = reach[canvasVariables.handlingDropdown.value];
+
+        // Endurance
+        playerController.maxEnergy = maxEnergy[canvasVariables.enduranceDropdown.value];
+        playerController.energyRecovery = energyRecovery[canvasVariables.enduranceDropdown.value];
+    }
+}
