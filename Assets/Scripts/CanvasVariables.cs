45f0021e3f4b221849261a173b0cf6bb7bd047bb
Matthew Morris <MRRMAT011@myuct.ac.za>
2020-04-29 17:11:36 +0200

    Name selection

---
+++ Assets/Scripts/CanvasVariables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CanvasVariables : MonoBehaviour
+{
+    public TMP_Dropdown teamDropdown;
+    public TMP_InputField nameField;
+    public GameObject pregameOptions;
+}
---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,41 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : NetworkBehaviour
+{
+    public TextMesh playerNameText;
+
+    private CanvasVariables canvasVariables;
+
+    [SyncVar(hook = nameof(SetPlayerName))]
+    public string playerName = "Player";
+
+    void Start()
+    {
+        if (!isLocalPlayer) return;
+        canvasVariables = GameObject.Find("/GameControl").GetComponent<CanvasVariables>();
+
+        // Execute command to set name if not blank
+        if (canvasVariables.nameField.text != "")
+        {
+            CmdSetName(canvasVariables.nameField.text);
+        }
+
+        canvasVariables.pregameOptions.SetActive(false);
+    }
+
+    // Sync var updated
+    void SetPlayerName(string oldValue, string newValue)
+    {
+        playerNameText.text = newValue;
+    }
+
+    // Command to set the name in the server
+    [Command]
+    public void CmdSetName(string newPlayerName)
+    {
+        playerName = newPlayerName;
+    }
+}
